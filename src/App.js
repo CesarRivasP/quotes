dddc466c5d5b5ede6f13c4be0679a5a0c480de9c
CesarRivasP <cesarjose58@gmail.com>
2019-06-15 19:35:58 -0400

    Added css classes and implemented form component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
+import Form from './components/form';
 
 
 function App() {
   // useState
   const [quote, updateQuote] = useState([]);
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <Fragment>
+      <h1>Administrador de Pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Form />
+          </div>
+          <div className="one-half column">
+
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
