89b713fbf05afa7234f312d4ed734c2810ea3dd9
CesarRivasP <cesarjose58@gmail.com>
2019-06-16 11:09:21 -0400

    Delete fragment component

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,19 +1,16 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 
 function Quote({ quote, index, deleteQuote }) {
   return(
-    <Fragment>
-      <h2 className="quote-title">Citas</h2>
-      <div className="cita">
-        <p>Paciente: <span>{ quote.patient }</span></p>
-        <p>Fecha: <span>{ quote.date }</span></p>
-        <p>Hora: <span>{ quote.time }</span></p>
-        <p>Sintomas: <span>{ quote.symptom }</span></p>
+    <div className="cita">
+      <p>Paciente: <span>{ quote.patient }</span></p>
+      <p>Fecha: <span>{ quote.date }</span></p>
+      <p>Hora: <span>{ quote.time }</span></p>
+      <p>Sintomas: <span>{ quote.symptom }</span></p>
 
-        <button onClick={() => deleteQuote(index)} type="button" className="u-full-width button-delete X">Eliminar</button>
-      </div>
-    </Fragment>
+      <button onClick={() => deleteQuote(index)} type="button" className="u-full-width button-delete X">Eliminar</button>
+    </div>
   );
 }
 
