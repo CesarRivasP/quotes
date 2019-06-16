389ce137fce0be9932dcc7ca519a5dacb83546ac
CesarRivasP <cesarjose58@gmail.com>
2019-06-16 11:08:48 -0400

    IMplemented localStorage and conditional title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import Form from './components/form';
 import Quote from './components/quote';
 
 
 function App() {
+
+  // cargar las citas de localStorage como state inicial
+  let initialsQuotes = JSON.parse(localStorage.getItem('quotes'));
+
+  if(!initialsQuotes){
+    // En caso de que no hayan citas
+    initialsQuotes = [];
+  }
+
   // useState
-  const [quotes, updateQuote] = useState([]);
+  const [quotes, updateQuote] = useState(initialsQuotes);
 
   // Agregar las nuevas citas
   const createQuote = (quote) => {
@@ -22,15 +31,37 @@ function App() {
     updateQuote(newQuotes);
   }
 
+  useEffect(
+    () => {
+      console.log('Component listo o algo cambio');
+      let initialsQuotes = JSON.parse(localStorage.getItem('quotes'));  //quotes viene como string y hay que parsearlo
+
+      if(quotes){
+        //Cuando haya citas, que las guarde en localStorage
+        localStorage.setItem('quotes', JSON.stringify(quotes))
+      }
+      else {
+        // Para la primera vez, que no haya nada
+        localStorage.setItem('quotes', JSON.stringify([]))
+      }
+    }, [quotes] //para que se ejecuta unicamente cuando las citas tengan algun cambio
+  )
+
+  // Cargar condicionalmente un titulo
+  const title = Object.keys(quotes).length === 0 ? 'No hay Citas' : 'Administrar Citas';
+
   return (
     <Fragment>
-      <h1>Administrador de Pacientes</h1>
+      <header className="header-container">
+        <h1>Administrador de Pacientes</h1>
+      </header>
       <div className="container">
         <div className="row">
           <div className="one-half column">
             <Form createQuote={createQuote} />
           </div>
           <div className="one-half column">
+            <h2 className="quote-title">{ title }</h2>
             {
               quotes.map((quote, index) => (
                 <Quote key={index} index={index} quote={quote} deleteQuote={deleteQuote} />
