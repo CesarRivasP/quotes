10ac8fc402533a98186bf914aad53bb53e181677
CesarRivasP <cesarjose58@gmail.com>
2019-06-15 22:08:42 -0400

    Added deleteQuote function and new title

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 
-function Quote({ quote }) {
+function Quote({ quote, index, deleteQuote }) {
   return(
-    <div className="cita">
-      <p>Paciente: <span>{ quote.patient }</span></p>
-      <p>Fecha: <span>{ quote.date }</span></p>
-      <p>Hora: <span>{ quote.time }</span></p>
-      <p>Sintomas: <span>{ quote.symptom }</span></p>
-    </div>
+    <Fragment>
+      <h2 className="quote-title">Citas</h2>
+      <div className="cita">
+        <p>Paciente: <span>{ quote.patient }</span></p>
+        <p>Fecha: <span>{ quote.date }</span></p>
+        <p>Hora: <span>{ quote.time }</span></p>
+        <p>Sintomas: <span>{ quote.symptom }</span></p>
+
+        <button onClick={() => deleteQuote(index)} type="button" className="u-full-width button-delete X">Eliminar</button>
+      </div>
+    </Fragment>
   );
 }
 
